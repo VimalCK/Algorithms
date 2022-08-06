06f76e33d7f9e22634d98b5ee1ac73a555b1281b
Vimal CK <vimal-ck@outlook.com>
2022-08-06 01:32:17 +0100

    Find position of an element in an array

---
+++ Algorithms/LeetCode/Medium/FindPostionOfElementInArray.cs
@@ -0,0 +1,40 @@
+﻿using Algorithms.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Algorithms.LeetCode.Medium
+{
+    /*
+     * Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+     * If target is not found in the array, return [-1, -1].
+     * You must write an algorithm with O(log n) runtime complexity.
+     */
+
+    public class FindPostionOfElementInArray : TheoryData<int[], int, int[]>
+    {
+        public FindPostionOfElementInArray()
+        {
+            Add(new int[] { 5, 7, 7, 8, 8, 10 }, 8, new int[] { 3, 4 });
+            Add(new int[] { 5, 7, 7, 8, 8, 10 }, 6, new int[] { -1, -1 });
+            Add(new int[] { }, 0, new int[] { -1, -1 });
+        }
+
+        [Theory]
+        [ClassData(typeof(FindPostionOfElementInArray))]
+        public void FindPositonOfElementInASortedArray(int[] data, int target, int[] expectedResult)
+        {
+            var result = SearchRange(data, target);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        public int[] SearchRange(int[] nums, int target)
+        {
+
+        }
+    }
+}
---
+++ Algorithms/LeetCode/Medium/ValidateBinarySearchTree.cs
@@ -0,0 +1,62 @@
+﻿using Algorithms.Types;
+using System;
+using Xunit;
+
+namespace Algorithms.LeetCode.Medium
+{
+    /*
+     * Given the root of a binary tree, determine if it is a valid binary search tree (BST). 
+     * A valid BST is defined as follows:
+     *      The left subtree of a node contains only nodes with keys less than the node's key.
+     *      The right subtree of a node contains only nodes with keys greater than the node's key.
+     *      Both the left and right subtrees must also be binary search trees.
+     *      
+     *              Correct                 Incorrect
+     *              
+     *                  2                       5
+     *                 / \                     / \
+     *                1   3                   1   4
+     *                                           / \
+     *                                          3   6
+     */
+    public class ValidateBinarySearchTree : TheoryData<TreeNode, bool>
+    {
+        public ValidateBinarySearchTree()
+        {
+            Add(new TreeNode(new int?[] { 5, 4, 6, null, null, 3, 7 }), false);
+            Add(new TreeNode(new int?[] { 0 }), true);
+            Add(new TreeNode(new int?[] { 2, 2, 2 }), false);
+            Add(new TreeNode(new int?[] { 2, 1, 3 }), true);
+            Add(new TreeNode(new int?[] { 5, 1, 4, null, null, 3, 6 }), false);
+        }
+
+        [Theory]
+        [ClassData(typeof(ValidateBinarySearchTree))]
+        public void ReturnTrueIfBinarySearchTreeIsValid(TreeNode node, bool expectedResult)
+        {
+            var result = IsValidBST(node);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        public bool IsValidBST(TreeNode? root)
+        {
+            return IsValid(root, null, null);
+        }
+
+        public bool IsValid(TreeNode? root, int? high, int? low)
+        {
+            if (root is null)
+            {
+                return true;
+            }
+
+            if ((high != null && root.val >= high) || (low != null && root.val <= low))
+            {
+                return false;
+            }
+
+            return IsValid(root.left, root.val, low) && IsValid(root.right, high, root.val);
+        }
+    }
+}
