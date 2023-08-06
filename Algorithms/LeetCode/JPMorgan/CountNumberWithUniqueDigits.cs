6eec0bbbdc9df1bc5cdc4a7eb4c9643292344d47
Vimal CK <vimal-ck@outlook.com>
2023-08-07 00:36:05 +0100

    CountNumberWithUniqueDigits

---
+++ Algorithms/LeetCode/JPMorgan/CountNumberWithUniqueDigits.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Algorithms.LeetCode.JPMorgan
+{
+    public class CountNumberWithUniqueDigits : TheoryData<int, int>
+    {
+        /* Given an integer n, return the count of all numbers with unique digits, x, where 0 <= x < 10n. */
+
+        public CountNumberWithUniqueDigits()
+        {
+            Add(0, 1);
+            Add(1, 10);
+            Add(2, 91);
+            Add(3, 739);
+            Add(4, 5275);
+        }
+
+        [Theory]
+        [ClassData(typeof(CountNumberWithUniqueDigits))]
+        public void CheckCountNumbersWithUniqueDigits(int n, int expectedResult)
+        {
+            var result = CountNumbersWithUniqueDigits(n);
+
+            Assert.Equal(result, expectedResult);
+        }
+
+        public int CountNumbersWithUniqueDigits(int n)
+        {
+            int sum = 1;
+            int product = 10;
+            if (n >= 1)
+            {
+                sum = 10;
+            }
+
+            for (int i = 1; i < n; i++)
+            {
+
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Algorithms/LeetCode/JPMorgan/GenerateParentheses.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Algorithms.LeetCode.JPMorgan
+{
+    /* Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses. */
+
+    public class GenerateParentheses : TheoryData<int, List<string>>
+    {
+        public GenerateParentheses()
+        {
+            Add(3, new List<string>(new[] { "((()))", "(()())", "(())()", "()(())", "()()()" }));
+            Add(1, new List<string>(new[] { "()" }));
+        }
+
+        [Theory]
+        [ClassData(typeof(GenerateParentheses))]
+        public void ParenthesesAreGenerated(int count, List<string> expectedResult)
+        {
+            var result = GenerateParenthesis(count);
+
+            Assert.True(result.AreEqual(expectedResult));
+        }
+
+        public IList<string> GenerateParenthesis(int n)
+        {
+           
+            return default;
+        }
+
+    }
+}
